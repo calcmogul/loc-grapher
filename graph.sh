de039509f410d78d194de84ee0b022bd489c1b20
Tyler Veness <calcmogul@gmail.com>
2016-11-20 08:09:52 -0800

    Made small modifications during performance testing
    git rev-list is now used to retrieve the total number of commits and the extension regex was tweaked.

--- graph.sh
+++ graph.sh
@@ -5,14 +5,16 @@ branch=master
 
 repo=$(basename $url)
 
-# Clone updated Git repository to /tmp
 pushd $PWD
 cd /tmp
+
 if [ ! -d "$repo" ]; then
+  # Clone Git repository to /tmp
   git clone $url
   cd $repo
   git checkout -q -f $branch
 else
+  # Update Git repository in /tmp
   cd $repo
   git checkout -q -f $branch
   git pull
@@ -25,14 +27,14 @@ rm -f data.csv
 # Write header to CSV
 echo commits$','loc >> data.csv
 
-declare -i total=$(git log --pretty=oneline | wc -l)
+declare -i total=$(git rev-list --count $branch)
 
 # Get lines of code (LOC) for all commits in branch and write it to CSV
 for elem in `git log --reverse --pretty=oneline | cut -d ' ' -f 1`; do
   # Get LOC for current commit
-  git checkout -q $elem
+  git checkout -q -f $elem
   x=$((x + 1))
-  y=$(find $PWD -type f -regextype posix-extended -regex '.*(\.cpp|\.hpp|\.inl|\.h|\.c|\.java)$' -exec cat {} + | wc -l)
+  y=$(find $PWD -type f -regextype posix-extended -regex '.*\.(cpp|hpp|inl|h|c|java)$' -exec cat {} + | wc -l)
   percent=$((x * 100 / total))
 
   # Print progress data and write LOC count to CSV
