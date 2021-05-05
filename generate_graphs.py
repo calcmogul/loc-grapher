5c3db14d8bd95bea3e75e5e1d1addcfcbd8554b8
Tyler Veness <calcmogul@gmail.com>
2021-05-04 19:44:04 -0700

    Add plots for 2019 through 2021 offseasons

--- generate_graphs.py
+++ generate_graphs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import re
 import shutil
@@ -28,55 +29,65 @@ def clone_repo(url, branch):
 
 
 def main():
-    branch = "main"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-latex-only",
+        dest="latex_only",
+        action="store_true",
+        help="Don't run CSV generation before LaTeX compilation",
+    )
+    args = parser.parse_args()
+
+    branch = "2022"
 
     cwd = os.getcwd()
     os.chdir(tempfile.gettempdir())
     clone_repo("git://github.com/wpilibsuite/allwpilib", branch)
 
-    # Create list of commit hashes
-    print("Collecting commit data...", end="")
-    sys.stdout.flush()
-    args = [
-        "git",
-        "--no-pager",
-        "log",
-        "--numstat",
-        "--reverse",
-        "--pretty=oneline",
-        branch,
-    ]
-    output_list = subprocess.check_output(args, encoding="utf-8").splitlines()
-    print(" done.")
+    if not args.latex_only:
+        # Create list of commit hashes
+        print("Collecting commit data...", end="")
+        sys.stdout.flush()
+        args = [
+            "git",
+            "--no-pager",
+            "log",
+            "--numstat",
+            "--reverse",
+            "--pretty=oneline",
+            branch,
+        ]
+        output_list = subprocess.check_output(args, encoding="utf-8").splitlines()
+        print(" done.")
 
-    line_regex = re.compile("([0-9]+)\s+([0-9]+)\s+(\S+)")
-    ext_regex = re.compile("\.(c|cpp|h|hpp|inc|inl|java|py)$")
-    commit_count = 0
-    line_count = 0
-    with open("data.csv", "w") as data:
-        for line in output_list:
-            if "\t" not in line[:40]:
-                # Write entry to file
-                data.write(str(commit_count))
-                data.write(",")
-                data.write(str(line_count))
-                data.write("\n")
+        line_regex = re.compile("([0-9]+)\s+([0-9]+)\s+(\S+)")
+        ext_regex = re.compile("\.(c|cpp|h|hpp|inc|inl|java|py)$")
+        commit_count = 0
+        line_count = 0
+        with open("data.csv", "w") as data:
+            for line in output_list:
+                if "\t" not in line[:40]:
+                    # Write entry to file
+                    data.write(str(commit_count))
+                    data.write(",")
+                    data.write(str(line_count))
+                    data.write("\n")
 
-                commit_count += 1
-            else:
-                match = line_regex.search(line)
-                if match and ext_regex.search(match.group(3)):
-                    line_count += int(match.group(1))
-                    line_count -= int(match.group(2))
-        # Write rest of data
-        data.write(str(commit_count))
-        data.write(",")
-        data.write(str(line_count))
-        data.write("\n")
+                    commit_count += 1
+                else:
+                    match = line_regex.search(line)
+                    if match and ext_regex.search(match.group(3)):
+                        line_count += int(match.group(1))
+                        line_count -= int(match.group(2))
+            # Write rest of data
+            data.write(str(commit_count))
+            data.write(",")
+            data.write(str(line_count))
+            data.write("\n")
+        shutil.move("data.csv", f"{cwd}/loc/data.csv")
 
     # Generate graphs
-    shutil.move("data.csv", cwd + "/loc/data.csv")
-    os.chdir(cwd + "/loc")
+    os.chdir(f"{cwd}/loc")
     subprocess.run(["latexmk", "-pdf", "-silent", "loc"])
     shutil.copy("loc.pdf", "../loc.pdf")
 
